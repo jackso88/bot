6ba1e698e7f1d33b6a9ca862af3b851d89b1af91
Andrei <andreilukin88@gmail.com>
2020-11-20 18:48:38 +0300

    finish

--- bot.py
+++ bot.py
@@ -4,33 +4,35 @@ import re
 from bs4 import BeautifulSoup as bs
 
 
-bot = telebot.TeleBot('1327213027:AAGcwZpzOAqbXZCpfEnaoBwM5Ti7lEsQ86E')
+bot = telebot.TeleBot('')
+
 
 @bot.message_handler(content_types=['text'])
-
 def get_text_messages(message):
-	if message.text == '/start':
-		bot.send_message(message.from_user.id, "Hi! What do you want to find on Merriam-Webster dictionary?")
-	elif ' ' in message.text:
-		bot.send_message(message.from_user.id, 'Enter only one word, pls')
-	else:
-		url = "https://www.merriam-webster.com/dictionary/" + message.text
-		r = requests.get(url)
-		html = r.content
-		soup = bs(html, "html.parser")
-		text = 'Defenition of ' + str(message.text).capitalize() + ': '
-		for meta in soup.find_all('meta'):
-			prop = meta.get('property')
-			if prop == 'og:description':
-				text = text + str(meta.get('content'))
-				text = text.replace('See the full definition', '') + "\n\nExample of using in sentence:\n\n"
-		for data in soup.find_all('span', class_= 't has-aq'):
-			text = text + ' ' + str(data) + '\n\n'
-		bot.send_message(message.from_user.id, re.sub(r'<.*?>','',text).replace('</*>','') + ' ' + url)
+    if message.text == '/start':
+        txt = "Hi! What do you want to find on Merriam-Webster"
+        txt += " dictionary?"
+        bot.send_message(message.from_user.id, txt)
+    elif ' ' in message.text:
+        txt = 'Enter only one word, pls'
+        bot.send_message(message.from_user.id, txt)
+    else:
+        url = "https://www.merriam-webster.com/dictionary/"
+        url += message.text
+        r = requests.get(url)
+        html = r.content
+        soup = bs(html, "html.parser")
+        text = 'Defenition of ' + str(message.text).capitalize() + ': '
+        for meta in soup.find_all('meta'):
+            prop = meta.get('property')
+            if prop == 'og:description':
+                text = text + str(meta.get('content'))
+                text = text.replace('See the full definition', '')
+                text += "\n\nExample of using in sentence:\n\n"
+        for data in soup.find_all('span', class_='t has-aq'):
+            text = text + ' ' + str(data) + '\n\n'
+        mssg = message.from_user.id, re.sub(r'<.*?>', '', text).replace('</*>', '') + ' ' + url
+        bot.send_message(mssg)
 
-		
 
- 
 bot.polling(none_stop=True, interval=0)
-
-
